5d289048b0e261d27b240b413ac9c1bbb2c13d9e
Kiyonaga Hikaru <magicsirotama@gmail.com>
2017-08-08 11:28:57 +0900

    🗑  いらんもんを全部消した

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <h1>{{ msg }}</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data () {
+    return {
+      msg: 'Misskey-Frontend'
+    }
+  }
 }
 </script>
 
@@ -18,5 +23,23 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
 }
 </style>
