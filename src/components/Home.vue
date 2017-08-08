0a4c07edd15026b45fe42dcffc155ff1e4d3bb74
Kiyonaga Hikaru <magicsirotama@gmail.com>
2017-08-08 11:00:29 +0900

    mock

---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Misskey-Frontend'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
