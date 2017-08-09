a582faaf4af0539d084c4b3738e484b87a72d91f
Kiyonaga Hikaru <magicsirotama@gmail.com>
2017-08-09 12:29:42 +0900

    モック2

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App'
+import router from './router'
+import Home from './web/Home'
+import { sync } from 'vuex-router-sync'
 
+sync(Home, router)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
 new Vue({
   el: '#app',
-  render: h => h(App)
+  router,
+  Home,
+  template: '<App/>',
+  components: { App }
 })
